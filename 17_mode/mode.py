f431cd249971072449bb81c59d48a2a239fa3606
Javier Quiroga <javier.quiroga.e@gmail.com>
2022-12-28 05:29:17 -0500

    15,16,17,18

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,12 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    frequencies = [nums.count(n) for n in nums]
+    max_frequency = 0
+    
+    for x in frequencies:
+        if x > max_frequency:
+            max_frequency = x
+    index_of_max_frequency = frequencies.index(max_frequency)
+
+    return nums[index_of_max_frequency]
--- 18_calculate/calculate.py
+++ 18_calculate/calculate.py
@@ -26,3 +26,25 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
         >>> calculate('foo', 2, 3)
         
     """
+    if operation == 'add':
+        if make_int:
+            return message + f" {int(a+b)}"
+        else:
+            return message + f" {a+b}"
+    elif operation == 'subtract':
+        if make_int:
+            return message + f" {int(a-b)}"
+        else:
+            return message + f" {a-b}"
+    elif operation == 'multiply':
+        if make_int:
+            return message + f" {int(a*b)}"
+        else:
+            return message + f" {a*b}"
+    elif operation == 'divide':
+        if make_int:
+            return message + f" {int(a/b)}"
+        else:
+            return message + f" {a/b}"
+    else:
+        return None
