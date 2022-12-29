b132a5a47183e9fc216d988e5578c46a121a4bb3
Javier Quiroga <javier.quiroga.e@gmail.com>
2022-12-28 21:09:49 -0500

    31

--- 31_truncate/truncate.py
+++ 31_truncate/truncate.py
@@ -24,3 +24,13 @@ def truncate(phrase, n):
         >>> truncate("Woah", 3)
         '...'
     """
+    if n < 3:
+        return 'Truncation must be at least 3 characters.'
+    elif n ==3:
+        return '...'
+    elif len(phrase) < n:
+        new_phrase = phrase[:n-3]
+        return new_phrase
+    else:
+        new_phrase = phrase[:n-3]
+        return new_phrase + '...'
